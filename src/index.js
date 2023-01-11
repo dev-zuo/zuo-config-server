1ae9bc712cbe4688172809484ae8ac2340f0bb47
zuoxiaobai <guoqzuo@gmail.com>
2023-01-12 00:28:43 +0800

    feat(v1.0.0): 新增列表接口分页、模糊查询逻辑

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = new Koa();
 const router = new KoaRouter();
 const mongodbCore = require("./mongodb/mongodb.js");
-const { shortLinkDb } =  require('./mongodb/dao.js')
+const { shortLinkDb } = require("./mongodb/dao.js");
 
 app.use(bodyParser()); // 处理 post 请求参数
 
@@ -15,12 +15,12 @@ app.use(bodyParser()); // 处理 post 请求参数
  * 作者：一只正在成长的程序猿
  * 链接：https://juejin.cn/post/6844904042196533255
  */
-app.use(async (ctx, next)=> {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', '*');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (ctx.method == 'OPTIONS') {
-    ctx.body = 200; 
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set("Access-Control-Allow-Headers", "*");
+  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  if (ctx.method == "OPTIONS") {
+    ctx.body = 200;
   } else {
     await next();
   }
@@ -32,73 +32,77 @@ app.use(async (ctx, next)=> {
 // app.use(mpServer.verifyToken()); // token 校验中间件
 
 // 连接 mongodb，初始化 db
-mongodbCore.init({dbName: 'zuo-config'})
+mongodbCore.init({ dbName: "zuo-config" });
 
 router.get("/shortLink/list", async (ctx) => {
-    console.log('ctx.query', ctx.query)
-    try {
-      let list = await shortLinkDb.getList()
-      ctx.body = {
-        code: 0,
-        data: {
-          queryText: ctx.query.queryText,
-          list,
-          total: 0
-        },
-        msg: '成功',
-      };
-    } catch(e) {
-      ctx.body = {code: -10001, msg: '获取短链接列表失败', plainMsg: e.message };
-    }
+  console.log("ctx.query", ctx.query);
+  let { queryText, currentPage, pageSize } = ctx.query;
+  try {
+    let {list, total} = await shortLinkDb.getList(queryText, {
+      pageSize: parseInt(pageSize),
+      pageIndex: parseInt(currentPage),
+    });
+    ctx.body = {
+      code: 0,
+      data: {
+        queryText: ctx.query.queryText,
+        list,
+        total
+      },
+      msg: "成功",
+    };
+  } catch (e) {
+    ctx.body = { code: -10001, msg: "获取短链接列表失败", plainMsg: e.message };
+  }
 });
 
 router.post("/shortLink/add", async (ctx) => {
-  console.log(ctx.request.body)
+  console.log(ctx.request.body);
   try {
-    let insertResult= await shortLinkDb.add(ctx.request.body)
+    let insertResult = await shortLinkDb.add(ctx.request.body);
     ctx.body = {
       code: 0,
       data: {
-        insertResult
+        insertResult,
       },
-      msg: '成功',
+      msg: "成功",
     };
-  } catch(e) {
-    ctx.body = {code: -10001, msg: '新增短链接配置失败', plainMsg: e.message };
+  } catch (e) {
+    ctx.body = { code: -10001, msg: "新增短链接配置失败", plainMsg: e.message };
   }
-})
+});
 
 router.post("/shortLink/edit", async (ctx) => {
-  console.log(ctx.request.body)
+  console.log(ctx.request.body);
   try {
-    let updateResult = await shortLinkDb.edit(ctx.request.body)
+    let updateResult = await shortLinkDb.edit(ctx.request.body);
     ctx.body = {
       code: 0,
       data: {
-        updateResult
+        updateResult,
       },
-      msg: '成功',
+      msg: "成功",
     };
-  } catch(e) {
-    ctx.body = {code: -10001, msg: '修改短链接配置失败', plainMsg: e.message };
+  } catch (e) {
+    ctx.body = { code: -10001, msg: "修改短链接配置失败", plainMsg: e.message };
   }
-})
+});
 
 router.post("/shortLink/del", async (ctx) => {
-  let { id } = ctx.request.body
+  let { id } = ctx.request.body;
   try {
-    let deleteResult = await shortLinkDb.del(id)
+    let deleteResult = await shortLinkDb.del(id);
     ctx.body = {
       code: 0,
       data: {
-        deleteResult
+        deleteResult,
       },
-      msg: '成功',
+      msg: "成功",
     };
-  } catch(e) {
-    ctx.body = {code: -10001, msg: '删除短链接配置失败', plainMsg: e.message };
+  } catch (e) {
+    ctx.body = { code: -10001, msg: "删除短链接配置失败", plainMsg: e.message };
   }
-})
+});
 
 // router.get("/goods/get", async (ctx) => {
 //   ctx.body = {
@@ -111,10 +115,10 @@ router.post("/shortLink/del", async (ctx) => {
 // });
 
 router.post("/user/edit", async (ctx) => {
-    ctx.body = {
-      code: 0,
-      msg: '修改成功',
-    };
+  ctx.body = {
+    code: 0,
+    msg: "修改成功",
+  };
 });
 
 app.use(router.routes()).use(router.allowedMethods());
--- src/mongodb/dao.js
+++ src/mongodb/dao.js
@@ -1,44 +1,66 @@
-const mongodbCore = require('../mongodb/mongodb.js')
-const { ObjectId} = require('mongodb')
+const mongodbCore = require("../mongodb/mongodb.js");
+const { ObjectId } = require("mongodb");
 
 const shortLinkDb = {
-    async getList() {
-        const db = mongodbCore.getDb()
-        // 查询所有数据
-        const data = await db.collection('short-link').find({}).toArray();
-        return data
-    },
-
-    async add(payload) {
-        const db = mongodbCore.getDb()
-         // 插入数据时 _id 会自动增加
-         const insertResult = await db.collection('short-link').insertMany([
-            payload
-        ]);
-        return insertResult
-    },
-
-    async edit(payload) {
-        const db = mongodbCore.getDb()
-        const { shortLink, redirect, _id } = payload
-        const updateResult = await db.collection('short-link').updateOne({
-            _id: ObjectId(_id)
-        }, {
-            $set: { shortLink, redirect }
-        })
-        return updateResult
-    },
-    
-    async del(id) {
-        const db = mongodbCore.getDb()
-        const deleteResult = await db.collection('short-link').deleteMany({
-            _id: ObjectId(id)
-        })
-        return deleteResult
+  async getList(queryStr, { pageIndex, pageSize }) {
+    const db = mongodbCore.getDb();
+    // 查询列表数据
+    let queryRule = {};
+    if (queryStr) {
+      queryRule = {
+        $or: [
+          { shortLink: { $regex: queryStr } },
+          { redirect: { $regex: queryStr } },
+        ],
+      };
     }
-}
+    // 页码 - skip数据量 - 公式
+    // 1 0  (pageIndex - 1) * pageSize
+    // 2 pageSize
+    // 3
+    const list = await db
+      .collection("short-link")
+      .find(queryRule)
+      .limit(pageSize)
+      .skip((pageIndex - 1) * pageSize)
+      .toArray();
 
+    const total = await db.collection("short-link").find(queryRule).count();
+    return {list, total};
+  },
+
+  async add(payload) {
+    const db = mongodbCore.getDb();
+    // 插入数据时 _id 会自动增加
+    const insertResult = await db
+      .collection("short-link")
+      .insertMany([payload]);
+    return insertResult;
+  },
+
+  async edit(payload) {
+    const db = mongodbCore.getDb();
+    const { shortLink, redirect, _id } = payload;
+    const updateResult = await db.collection("short-link").updateOne(
+      {
+        _id: ObjectId(_id),
+      },
+      {
+        $set: { shortLink, redirect },
+      }
+    );
+    return updateResult;
+  },
+
+  async del(id) {
+    const db = mongodbCore.getDb();
+    const deleteResult = await db.collection("short-link").deleteMany({
+      _id: ObjectId(id),
+    });
+    return deleteResult;
+  },
+};
 
 module.exports = {
-    shortLinkDb
-}
+  shortLinkDb,
+};
